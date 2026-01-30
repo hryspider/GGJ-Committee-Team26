bfb7c4c6fb1780c07614dd952fc155f5a0cd0181
angtaming <62770543+angtaming@users.noreply.github.com>
2026-01-30 20:45:27 +0000

    glorp can move

---
+++ glorp.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var speed = 400
+	
+	var direction = Input.get_vector("left", "right", "up", "down")
+	var velocity = direction * speed 
+
+	pass
