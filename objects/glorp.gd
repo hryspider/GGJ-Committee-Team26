b3e93a3eb4f507e07be53ab95af3fcefa0a15b93
Harry <peaty-danish0s@icloud.com>
2026-01-31 01:23:39 +0000

    glorp! masks and stuff

--- objects/glorp.gd
+++ objects/glorp.gd
@@ -1,6 +1,16 @@
-extends Node2D
+extends CharacterBody2D
+
+var speed = 200
+#var animation = "idle":
+	#set(value):
+		#if value != animation:
+			#animation = value
+			#sprite.animation = value
+
+
 @onready var sprite = $Sprite
 @onready var eye = $Sprite/Eye
+var eye_direction = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,10 +18,19 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	if direction:
+		sprite.play("walk")
+		sprite.flip_h = direction.x >= 0
+	else:
+		sprite.play("idle")
+	point_eye_to_mouse()
+	move_and_slide()
 
+func point_eye_to_mouse():
+	eye.offset = (eye.global_position - get_global_mouse_position()).normalized() * -2
 
 func _on_sprite_animation_changed():
 	eye.visible = sprite.animation == "idle"
