fe3a484fea544d7b1a27f67255ada1551b30e349
angtaming <62770543+angtaming@users.noreply.github.com>
2026-02-01 13:43:03 +0000

    2-3 screen

--- rooms/animated_sprite_2d.gd
+++ rooms/animated_sprite_2d.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if !get_node("../Glorp").is_emotion_valid():
-		suspicion += 0.7
+		suspicion += 0.5
 		suspicion = min(suspicion, 100)
 	else:
 		suspicion -= 0.1
