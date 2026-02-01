fb1db1e95fce6c9ff6d2452d5d33b73b518016e1
Harry <peaty-danish0s@icloud.com>
2026-02-01 00:44:37 +0000

    update speech bubble

--- objects/speech_bubble.gd
+++ objects/speech_bubble.gd
@@ -2,6 +2,9 @@ extends TextureRect
 
 @onready var runes := [$Control/Rune1, $Control/Rune2, $Control/Rune3, $Control/Rune4, $Control/Rune5, $Control/Rune6]
 var words = []
+var visible_chars = 0
+@onready var appear_timer = $AppearTimer
+@onready var destroy_timer = $DestroyTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,7 +28,23 @@ func refresh_text():
 		if i < len(words):
 			var new_text = LangaugeGlobals.player_dictionary.get(runes[i].get_rune_text(), "")
 			if new_text: runes[i].set_text_overlay(new_text)
-	
+
+func play():
+	set_visible_words(0)
+	appear_timer.start()
+
 func set_visible_words(n):
 	for i in range(runes.size()):
 		runes[i].visible = i<n
+
+
+func _on_appear_timer_timeout():
+	if visible_chars < 5:
+		visible_chars += 1
+		set_visible_words(visible_chars)
+	else:
+		appear_timer.stop()
+		destroy_timer.start()
+
+func _on_destroy_timer_timeout():
+	queue_free()
